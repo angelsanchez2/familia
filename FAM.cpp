8b2ec1203fae931ab463c6fa2f47973173a5b764
angelsanchez2 <65976049+angelsanchez2@users.noreply.github.com>
2020-06-02 13:22:25 -0500

    Add files via upload

---
+++ FAM.cpp
@@ -0,0 +1,406 @@
+
+#include <math.h>
+#include <stdio.h>
+
+int main()
+{
+    int n, opcion,E,O,x,V,Y,T,Q;
+
+    do
+    {
+		printf( "\n   BIENVENIDO A ARBOL GENEALOGIO ." );
+        printf( "\n   1. PRIMERA GENERACION (4TO ABUELO).");
+        printf( "\n   2. SEGUNDA GENERACION (TATARABUELO).");
+        printf( "\n   3. TERCERA GENERACION (BISABUELOS).");
+        printf( "\n   4. CUARTA GENERACION (ABUELOS).");
+        printf( "\n   5. QUINTA GENERACION (PADRES).");
+        printf( "\n   6. SEXTA GENERACION (YO).");
+        printf( "\n   7. Salir." );
+        printf( "\n\n   Introduzca opciocn (1-7): ");
+        scanf( "%d", &opcion );
+
+        /* Inicio del anidamiento */
+
+        switch ( opcion )
+        {
+            case 1: 
+				
+			
+ do
+    {
+printf( "\n   BIENVENIDO AL ARBOL GENEALOGICO DE SEGUNDA GENERACION: ");
+                      printf( "\n   1. FAMILIA PATERNA.");
+                      printf( "\n   2.FAMILIA MATERNA.");
+                        printf( "\n   3. Salir." );
+                           printf( "\n\n   Introduzca opcion (1-3): ");
+   
+
+        scanf( "%d", &Y );
+
+        /* Inicio del anidamiento */
+
+        switch ( Y )
+        {
+            case 1:
+
+
+printf( "\n   BIENVENIDO AL ARBOL GENEALOGICO DE SEGUNDA GENERACION FAMILIA PATERNA: ");
+printf( "\n  JUMERSINDO SANCHEZ  (TATARAABUELO)");
+printf( "\n  EULALIA DIMAS (TATARAABUELA)");
+
+
+
+  break;
+
+            case 2:
+
+printf( "\n   BIENVENIDO AL ARBOL GENEALOGICO DE TERCERA GENERACION MATERNA:" );
+printf( "\n  ROMAN RIVERA  (TATARAABUELO)");
+printf( "\n  JACINTA QUISTIAN (TATARAABUELA)");
+printf( "\n  BALDOMERO HERNANDEZ  (TATARAABUELO)");
+printf( "\n  APOLONIA VAZQUEZ (TATARAABUELA)");
+
+
+                                  
+         }
+
+         /* Fin del anidamiento */
+
+    } while ( Y!= 3 );
+             break;  
+           
+            case 2: 
+            
+            do
+    {
+printf( "\n   BIENVENIDO AL ARBOL GENEALOGICO DE SEGUNDA GENERACION: ");
+                      printf( "\n   1. FAMILIA PATERNA.");
+                      printf( "\n   2.FAMILIA MATERNA.");
+                        printf( "\n   3. Salir." );
+                           printf( "\n\n   Introduzca opcion (1-3): ");
+   
+
+        scanf( "%d", &Y );
+
+        /* Inicio del anidamiento */
+
+        switch ( Y )
+        {
+            case 1:
+
+
+printf( "\n   BIENVENIDO AL ARBOL GENEALOGICO DE SEGUNDA GENERACION FAMILIA PATERNA: ");
+printf( "\n  JUMERSINDO SANCHEZ  (TATARAABUELO)");
+printf( "\n  EULALIA DIMAS (TATARAABUELA)");
+
+
+
+  break;
+
+            case 2:
+
+printf( "\n   BIENVENIDO AL ARBOL GENEALOGICO DE TERCERA GENERACION MATERNA:" );
+printf( "\n  ROMAN RIVERA  (TATARAABUELO)");
+printf( "\n  JACINTA QUISTIAN (TATARAABUELA)");
+printf( "\n  BALDOMERO HERNANDEZ  (TATARAABUELO)");
+printf( "\n  APOLONIA VAZQUEZ (TATARAABUELA)");
+
+
+                                  
+         }
+
+         /* Fin del anidamiento */
+
+    } while ( V!= 3 );
+             break;  
+            
+                
+
+            case 3: 
+            
+            	do
+    {
+printf( "\n   BIENVENIDO AL ARBOL GENEALOGICO DE CUARTA GENERACION: ");
+                      printf( "\n   1. FAMILIA PATERNA.");
+                      printf( "\n   2.FAMILIA MATERNA.");
+                        printf( "\n   3. Salir." );
+                           printf( "\n\n   Introduzca opcion (1-3): ");
+   
+
+        scanf( "%d", &Q );
+
+        /* Inicio del anidamiento */
+
+        switch ( Q )
+        {
+            case 1:
+
+
+printf( "\n   BIENVENIDO AL ARBOL GENEALOGICO DE TERCERA GENERACION FAMILIA PATERNA: ");
+printf( "\n  FRANCISCO SANCHEZ HERNANDEZ  (BISABUELO)");
+printf( "\n  LORETO DIMAS ALMARAZ(BISABUELA)");
+printf( "\n   GLORIA (PRIMO)");
+printf( "\n   DOLORES (PRIMA)");
+printf( "\n   ROSARIO (PRIMO)");
+printf( "\n    (PRIMO)");
+printf( "\n   GRACIELA (PRIMA)");
+printf( "\n   MARIA (PRIMA)");
+printf( "\n   FELIPE (PRIMA)");
+printf( "\n   ASUNCION (PRIMA))");
+printf( "\n   MACARIO (PRIMO)");
+printf( "\n   MARGARITA (PRIMO)");
+printf( "\n   LETY (PRIMO)");
+printf( "\n   LUIS(PRIMO)");
+printf( "\n   ALBERTO (PRIMA)");
+printf( "\n   KAIN (PRIMA)");
+
+
+  break;
+
+            case 2:
+
+printf( "\n   BIENVENIDO AL ARBOL GENEALOGICO DE TERCERA GENERACION MATERNA:" );
+printf( "\n    PABLO RIVERA QUISTIAN(BISABUELO)");
+printf( "\n    BENITA VAZQUEZ HERNANDES(BISABUELA)");
+printf( "\n   CONCEPCION");
+printf( "\n   CARLOS");
+printf( "\n   ESTEBAN");
+printf( "\n   AMALIA");
+printf( "\n   GUADALUPE");
+printf( "\n   JOSE");
+printf( "\n   PABLO");
+
+                                  
+         }
+
+         /* Fin del anidamiento */
+
+    } while ( Q != 3 );
+             break;     
+                    
+            case 4: 
+            				do
+    {
+printf( "\n   BIENVENIDO AL ARBOL GENEALOGICO DE CUARTA GENERACION: ");
+                      printf( "\n   1. FAMILIA PATERNA.");
+                      printf( "\n   2.FAMILIA MATERNA.");
+                        printf( "\n   3. Salir." );
+                           printf( "\n\n   Introduzca opcion (1-3): ");
+   
+
+        scanf( "%d", &T );
+
+        /* Inicio del anidamiento */
+
+        switch ( T )
+        {
+            case 1:
+
+
+printf( "\n   BIENVENIDO AL ARBOL GENEALOGICO DE SEXTA GENERACION FAMILIA PATERNA: ");
+printf( "\n  ANGEL SANCHEZ DIMAS (ABUELO)");
+printf( "\n  CLUADIA RORANTES DELGADO (ABUELA)");
+printf( "\n   ALICIA (PRIMO)");
+printf( "\n   LUCIA (PRIMA)");
+printf( "\n   CARLOS (PRIMO)");
+printf( "\n   PEDRO (PRIMO)");
+printf( "\n   GRACIELA (PRIMA)");
+printf( "\n   MARIA (PRIMA)");
+printf( "\n   FELIPE (PRIMA)");
+printf( "\n   ASUNCION (PRIMA))");
+printf( "\n   MACARIO (PRIMO)");
+printf( "\n   MARGARITA (PRIMO)");
+printf( "\n   LETY (PRIMO)");
+printf( "\n   LUIS(PRIMO)");
+printf( "\n   ALBERTO (PRIMA)");
+printf( "\n   KAIN (PRIMA)");
+
+
+  break;
+
+            case 2:
+
+printf( "\n   BIENVENIDO AL ARBOL GENEALOGICO DE SEXTA GENERACION MATERNA:" );
+printf( "\n   JOSE ANTONIO RIVERA VAZQUEZ (ABUELO)");
+printf( "\n  MARIA ISABEL LAGUNAS OLIVEROS (ABUELA)");
+printf( "\n   BERTHA");
+printf( "\n   IGNACIO");
+printf( "\n   IMELDA");
+printf( "\n   APOLONIA");
+printf( "\n   RAUL");
+printf( "\n   JESUS");
+printf( "\n   ADRIANA");
+
+                                  
+         }
+
+         /* Fin del anidamiento */
+
+    } while ( T != 3 );
+             break;              
+				
+            case 5: 
+            do
+    {
+printf( "\n   BIENVENIDO AL ARBOL GENEALOGICO DE QUINTA GENERACION: ");
+                      printf( "\n   1. FAMILIA PATERNA.");
+                      printf( "\n   2.FAMILIA MATERNA.");
+                        printf( "\n   3. Salir." );
+                           printf( "\n\n   Introduzca opcion (1-3): ");
+   
+
+        scanf( "%d", &O );
+
+        /* Inicio del anidamiento */
+
+        switch ( O )
+        {
+            case 1:
+
+
+printf( "\n  JAZMIN ");
+printf( "\n    BARBARA");
+printf( "\n   ERICK");
+printf( "\n    EDUARDO");
+printf( "\n   ERIKA");
+printf( "\n    ELI");
+printf( "\n   RAUL");
+printf( "\n    GUADALUPE");
+printf( "\n   VICTORIA");
+printf( "\n    MARYSELA");
+printf( "\n   REYNA");
+printf( "\n   MARCELINA ");
+printf( "\n   ANGELINA");
+printf( "\n    SILVIA");
+printf( "\n   HECTOR");
+printf( "\n   JUAN");
+printf( "\n    ANTONIO");
+printf( "\n   JESSICA");
+printf( "\n    NOEMY");
+printf( "\n   ANTONIO");
+printf( "\n    JOAN");
+printf( "\n   GUADALUPE");
+printf( "\n    IVONNE");
+printf( "\n   NANCY");
+printf( "\n    MIRIAM");
+printf( "\n   DIANA");
+printf( "\n    LARISA");
+printf( "\n   EDUARDO");
+printf( "\n   JOSELYN ");
+printf( "\n   BERENICE");
+
+
+  break;
+
+            case 2:
+
+printf( "\n   BIENVENIDO AL ARBOL GENEALOGICO DE SEXTA QUINTA MATERNA:" );
+printf( "\n  GABRIELA RIVERA LAGUNAS (MADRE)");
+printf( "\n  JOSE (TIO)");
+printf( "\n  IVAN");
+printf( "\n   LUPE");
+printf( "\n   VERO");
+printf( "\n   GUSTAVI");
+printf( "\n   ULISES");
+printf( "\n   BELEN ");
+printf( "\n   BERE");
+printf( "\n   MARY");
+printf( "\n   KARLA");
+printf( "\n   PAOLA");
+printf( "\n   GEOVANY");
+printf( "\n   HECTOR");
+printf( "\n   ALEXIS");
+printf( "\n   PAOLA");
+printf( "\n   PEPE");
+printf( "\n    NANCY");
+printf( "\n   ALEJANDRO");
+printf( "\n    PABLO");
+printf( "\n   CRISTIAN");
+printf( "\n    ALONDRA");
+printf( "\n   MIGUEL");
+printf( "\n    JAZIEL");
+printf( "\n   JULIETA");
+printf( "\n    BERENICE");
+
+
+                                  
+         }
+
+         /* Fin del anidamiento */
+
+    } while ( O != 3 );
+             break;        
+					
+			case 6:
+				do
+    {
+printf( "\n   BIENVENIDO AL ARBOL GENEALOGICO DE SEXTA GENERACION: ");
+                      printf( "\n   1. FAMILIA PATERNA.");
+                      printf( "\n   2.FAMILIA MATERNA.");
+                        printf( "\n   3. Salir." );
+                           printf( "\n\n   Introduzca opcion (1-3): ");
+   
+
+        scanf( "%d", &E );
+
+        /* Inicio del anidamiento */
+
+        switch ( E )
+        {
+            case 1:
+
+
+printf( "\n   BIENVENIDO AL ARBOL GENEALOGICO DE SEXTA GENERACION FAMILIA PATERNA: ");
+printf( "\n  URIEL SANCHEZ RIVERA (YO)");
+printf( "\n  MICHEL SANCHEZ RIVERA (HERMANA)");
+printf( "\n   CRISTIAN (PRIMO)");
+printf( "\n   BRILLIT (PRIMA)");
+printf( "\n   YOHAN (PRIMO)");
+printf( "\n   DAVID (PRIMO)");
+printf( "\n   JEREMY (PRIMA)");
+printf( "\n   KATHERIN (PRIMA)");
+printf( "\n   BRITANI (PRIMA)");
+printf( "\n   ASHLYN (PRIMA))");
+printf( "\n   JAEL (PRIMO)");
+printf( "\n   JOHN (PRIMO)");
+printf( "\n   ULISES (PRIMO)");
+printf( "\n   BETO (PRIMO)");
+printf( "\n   SOFIA (PRIMA)");
+printf( "\n   MICHELLE (PRIMA)");
+
+
+  break;
+
+            case 2:
+
+printf( "\n   BIENVENIDO AL ARBOL GENEALOGICO DE SEXTA GENERACION MATERNA:" );
+printf( "\n  URIEL SANCHEZ RIVERA (YO)");
+printf( "\n  MICHEL SANCHEZ RIVERA (HERMANA)");
+printf( "\n   ANTONIO");
+printf( "\n   MITZY");
+printf( "\n   MARLY");
+printf( "\n   VALE");
+printf( "\n   DAVID");
+printf( "\n   SOFIA");
+printf( "\n   ROMINA");
+
+                                  
+         }
+
+         /* Fin del anidamiento */
+
+    } while ( E != 3 );
+             break;              
+				
+                
+			
+                    
+                    
+         }
+
+         /* Fin del anidamiento */
+
+    } while ( opcion != 7 );
+
+    return 0;
+}
